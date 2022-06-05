8ff9a937a3a4bc8ab4a7fb46e766133cde919b4d
mi nombre <desarrollowebaps@gmail.com>
2022-06-05 02:42:31 -0400

    Actualizando Pruebas js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  //FUNCIONES
-        //Indicar el mayor de 3 numeros ingresados por teclado
+        //1. Indicar el mayor de 3 numeros ingresados por teclado
         const numeroMayor = (dato1, dato2, dato3) =>{
             if(dato1 > dato2){
                 if(dato1 > dato3){
@@ -21,7 +21,7 @@
             }
         }
 
-        //Indicar el menor de 3 numeros ingresados por teclado
+        //2. Indicar el menor de 3 numeros ingresados por teclado
         const numeroMenor = (dato1, dato2, dato3)=>{
             if (dato1 < dato2){
                 if(dato1 < dato3){
@@ -43,7 +43,23 @@
             }
         }    
         
-       //Verificar si el numero introducido es par o impar
+       /*3. PROGRAMAR UNA FUNCION QUE CUENTE EL NUMERO DE CARACTERES DE UNA CADENA 
+       DE TEXTO PE MI FUNCION "HOLA MUNDO" DEVOLVERA 10*/
+         const contarCaracteres = (cadena)=>{
+             //let cadena = "HOLA MUNDO"
+             let numeroCaracteres = cadena.length
+             return numeroCaracteres  
+         } 
+
+         /*4. PROGRAMAR UNA FUNCION QUE TE DEVUELVA EL TEXTO RECORTADO SEGUN EL NUMERO DE 
+         CARACTERES INDICADOS PE. miFuncion("Hola Mundo", 4) devolvera "Hola"*/
+         const recortarTexto = () =>{
+             
+         }
+         
+         recortarTexto()
+    
+
 
 
 
@@ -64,5 +80,8 @@
                 let num2 = document.querySelector("#dato2").value
                 let num3 = document.querySelector("#dato3").value
                 mostrarResult.textContent = numeroMenor(num1, num2, num3)
+           }else if(e.target.id === "boton3"){
+                let caracter = document.querySelector("#caracter").value
+                mostrarResult.textContent = contarCaracteres(caracter)
            }
        })
